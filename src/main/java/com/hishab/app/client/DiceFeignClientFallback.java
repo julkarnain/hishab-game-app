96fd6521327441e43fb659ef85ab3763486a40c1
Julkar Nain <julkar.elixirsoft.nain@gmail.com>
2023-06-05 14:37:59 +0600

    Added Feign Client Rest

---
+++ src/main/java/com/hishab/app/client/DiceFeignClientFallback.java
@@ -0,0 +1,12 @@
+package com.hishab.app.client;
+
+import com.hishab.app.model.Dice;
+
+public class DiceFeignClientFallback implements DiceFeignClient {
+
+	@Override
+	public Dice rollDice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
