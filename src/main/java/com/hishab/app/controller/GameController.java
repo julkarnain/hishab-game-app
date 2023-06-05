678c9a00bd76093a56eca7519edef7b05159e23b
Julkar Nain <julkar.elixirsoft.nain@gmail.com>
2023-06-05 14:47:43 +0600

    Added controller classes

---
+++ src/main/java/com/hishab/app/controller/GameController.java
@@ -0,0 +1,92 @@
+package com.hishab.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hishab.app.constants.ApplicationConstants;
+import com.hishab.app.model.ResponseStatus;
+import com.hishab.app.model.ScoreBoard;
+import com.hishab.app.persistence.PersistenceDB;
+import com.hishab.app.service.DiceService;
+import com.hishab.app.service.GameService;
+import com.hishab.app.thread.CustomGameHandler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/1.0/game")
+public class GameController {
+
+	@Autowired
+	GameService gameService;
+
+	private final DiceService diceService;
+
+	@GetMapping(value = "/start/{gameId}")
+	public ResponseEntity<ResponseStatus> startGame(@PathVariable String gameId) {
+
+		ResponseStatus responseStatus = new ResponseStatus();
+
+		try {
+			
+			if (PersistenceDB.playerList.size() >= 2 && PersistenceDB.playerList.size() <= 4) {
+				
+				if (!ApplicationConstants.GAME_RUNNING_STATUS) {
+					
+					log.info("Game is starting.........#{}",gameId);
+
+					gameService.setUpPlayers();
+
+					CustomGameHandler customGameHandler = new CustomGameHandler(diceService,gameId);
+					Thread thread = new Thread(customGameHandler);
+					thread.start();
+					
+					/*
+					 * if(!ApplicationConstants.GAME_RUNNING_FlAG) { //thread.interrupt();
+					 * responseStatus.setStatusType("Normal"); responseStatus.setStatusCode("N001");
+					 * responseStatus.setStatusMessage("Game is finished successfully");
+					 * 
+					 * log.info("Game is finished. Please start again........."); }
+					 */
+
+					responseStatus.setStatusType("Normal");
+					responseStatus.setStatusCode("N001");
+					responseStatus.setStatusMessage("Game is started successfully");
+					
+				} else {
+					responseStatus.setStatusType("Normal");
+					responseStatus.setStatusCode("N002");
+					responseStatus.setStatusMessage("One Game is already running. Please wait until the Game is Over");
+					log.info("One Game #{} is already running.........",gameId);
+				}
+
+			} else {
+				responseStatus.setStatusType("Normal");
+				responseStatus.setStatusCode("E001");
+				responseStatus.setStatusMessage("Please fill-up/reduce player quota");
+			}
+		} catch (Exception ex) {
+			throw ex;
+		}
+
+		return new ResponseEntity<ResponseStatus>(responseStatus, HttpStatus.ACCEPTED);
+	}
+
+	@GetMapping(value = "/current-scoreboard")
+	public List<ScoreBoard> checkCurrentScoreBoard() {
+		return gameService.checkCurrentScoreBoard();
+	}
+
+}
---
+++ src/main/java/com/hishab/app/controller/PlayerController.java
@@ -0,0 +1,52 @@
+package com.hishab.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hishab.app.exceptionhandling.PlayerAlreadyExistsException;
+import com.hishab.app.exceptionhandling.PlayerNotFoundException;
+import com.hishab.app.model.Player;
+import com.hishab.app.service.PlayerService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@Slf4j
+@RequestMapping("/api/1.0/player")
+public class PlayerController {
+
+	@Autowired
+	PlayerService playerService;
+
+	@PostMapping(value = "/add")
+	public ResponseEntity<Player> addPlayer(@RequestBody Player player) throws PlayerAlreadyExistsException {
+
+		log.info("Start to add new player : {}", player.getName());
+
+		Player newPlayer = playerService.addPlayer(player);
+
+		log.info("End to add new player : {}", player.getName());
+
+		return new ResponseEntity<>(newPlayer, HttpStatus.CREATED);
+	}
+
+	@GetMapping(value = "/all")
+	public ResponseEntity<?> getPlayers() throws PlayerNotFoundException {
+
+		List<Player> players = playerService.getPlayers();
+
+		return new ResponseEntity<>(players, HttpStatus.OK);
+
+	}
+
+}
