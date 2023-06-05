c19299c69ac2503be6c034868807de8578848703
Julkar Nain <julkar.elixirsoft.nain@gmail.com>
2023-06-05 18:01:23 +0600

    Modified Repository and removed PlayerRepository Interface

--- src/test/java/com/hishab/app/client/DiceFeignClientTest.java
+++ src/test/java/com/hishab/app/client/DiceFeignClientTest.java
@@ -2,9 +2,6 @@ package com.hishab.app.client;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
